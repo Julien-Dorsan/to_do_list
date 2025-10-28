26ce13673e6182d9497bd46d3124902c27043dc2
Julien Dorsan <juliendorsan@gmail.com>
2025-10-28 14:34:04 +0100

    fix(apiClient): Les requêtes axios sont maintenant bien dirigées vers le backend.

--- frontend/src/api/apiClient.js
+++ frontend/src/api/apiClient.js
@@ -2,8 +2,9 @@ import axios from "axios";
 import config from "../../config"
 
 const apiClient = axios.create({
-    baseURL: config.apiBaseUrl,
+    baseURL: `${config.apiBaseUrl}/api`,
     // withCredentials: true,
+    headers: { Accept: 'application/json'}
 });
 
 export default apiClient;
