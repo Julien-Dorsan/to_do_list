df64614c534478c625e4163d24c41a0487595475
Julien Dorsan <juliendorsan@gmail.com>
2025-10-28 03:18:16 +0100

    feature(categories): Ajout d'une regex pour valider que la couleur est au format hexadecimal #RRGGBB

--- backend/categories/models.py
+++ backend/categories/models.py
@@ -1,9 +1,14 @@
 from django.db import models
+from django.core.validators import RegexValidator
 
+color_validator = RegexValidator(
+    regex=r"^#[0-9A-Fa-f]{6}$",
+    message="Color must be hex format (#RRGGBB)."
+)
 class Category(models.Model):
         name = models.CharField(max_length=30, null=False, unique=True)
         description = models.CharField(max_length=200)
-        color = models.CharField(max_length=7)
+        color = models.CharField(max_length=7, validators=[color_validator])
         created_at = models.DateTimeField(auto_now_add=True)
         updated_at = models.DateTimeField(auto_now=True)
         
